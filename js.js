a0bef954983ea7c8fc576c38cc70b24cce195562
CheeseFrog <au.yousef+dev@gmail.com>
2018-04-09 16:24:44 +1000

    Add files via upload

--- js.js
+++ js.js
@@ -1,15 +1,30 @@
 
 window.addEventListener("load",start);
+var y,x;
 function start() {
-var y=[],x=document.getElementsByTagName("span");
+//includeHTML()
+//x=document.getElementsByTagName("span");
+// function findold() {
+// for (var i=0;i<x.length;i++) if (x[i].style.background && x[i].innerText) {
+// 	y.push(x[i]);var n=str(x[i].innerText)
+// 	var l=document.getElementById(n);if (l) l.id="_";
+// 	x[i].id=n;x[i].onclick=scr;}}
 
-function find() {
-for (var i=0;i<x.length;i++) if (x[i].style.background && x[i].innerText) {
-	y.push(x[i]);var n=str(x[i].innerText)
-	var l=document.getElementById(n);if (l) l.id="_";
-	x[i].id=n;x[i].onclick=scr;}}
 function str(a) {return a.replace(/\s/g, "");}
 function scr() {location.hash = "#" + str(this.innerText);}
+function top() {location.hash="#Index:"}
+
+function find() {
+y=[];x=[52,66,79,13];for (var i=0;i<x.length;i++) y=y.concat([].slice.call(document.getElementsByClassName("c"+x[i])))
+
+for (var i=0;i<y.length;i++) {
+	var n=str(y[i].innerText)
+	var last=document.getElementById(n);
+	y[i].id=n;
+	y[i].onclick=scr;
+	if (last) {last.id="_"; y[i].onclick=top}
+	}
+}
 
 find();
 }

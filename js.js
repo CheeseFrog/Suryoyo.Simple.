67ff20522bc187e32023d27b4bdce632e299a68e
CheeseFrog <au.yousef+dev@gmail.com>
2018-04-10 11:39:02 +1000

    Add files via upload

--- js.js
+++ js.js
@@ -1,7 +1,38 @@
 
 window.addEventListener("load",start);
-var y,x,v=0;
+var y=[],x=[],v=0,l=[],l2=[];
+var style = document.createElement('style');
+style.type = 'text/css';
+
+
+
 function start() {
+
+function str(a) {return a.replace(/\s/g, "");}
+function scr() {location.hash="#"+str(this.innerText);}
+function top() {location.hash="";location.hash="#[Suryoyo.Simple.]";}//window.scrollTo(0,0);
+function invert() {v=!v;document.body.setAttribute("in",v)}
+function getindex() {
+var ss=document.styleSheets[1];
+var css=(ss.cssRules?ss.cssRules:ss.rules);
+for (var i=0;i<css.length;i++) {
+	if(css[i].style&&css[i].style.backgroundColor&&!css[i].style.padding) l.unshift(css[i].selectorText);}
+for (var i in l) l2.push(l[i]+":hover")
+style.innerHTML = l+"{cursor:pointer;user-select:none;} "+l2+"{outline:3px solid yellow;}"
+document.getElementsByTagName('head')[0].appendChild(style);}
+function find() {
+for (var i=0;i<l.length;i++) y=y.concat([].slice.call(document.getElementsByClassName(l[i].replace(/.c/g, "c"))))
+for (var i=0;i<y.length;i++) {
+	var n=str(y[i].innerText)
+	var last=document.getElementById(n);
+	y[i].id=n;
+	y[i].onclick=scr;
+	if (last) {last.id="_"; y[i].onclick=top}}}
+
+getindex();find();
+document.body.firstChild.firstChild.onclick=invert
+}
+
 //includeHTML()
 //x=document.getElementsByTagName("span");
 // function findold() {
@@ -9,25 +40,3 @@ function start() {
 // 	y.push(x[i]);var n=str(x[i].innerText)
 // 	var l=document.getElementById(n);if (l) l.id="_";
 // 	x[i].id=n;x[i].onclick=scr;}}
-
-function str(a) {return a.replace(/\s/g, "");}
-function scr() {location.hash = "#" + str(this.innerText);}
-function top() {location.hash="#INDEX:"}
-function invert() {v=!v;document.body.setAttribute("in",v)}
-
-function find() {
-document.getElementsByClassName("c28")[0].onclick=invert
-
-y=[];x=[71,82,21];for (var i=0;i<x.length;i++) y=y.concat([].slice.call(document.getElementsByClassName("c"+x[i])))
-
-for (var i=0;i<y.length;i++) {
-	var n=str(y[i].innerText)
-	var last=document.getElementById(n);
-	y[i].id=n;
-	y[i].onclick=scr;
-	if (last) {last.id="_"; y[i].onclick=top}
-	}
-}
-
-find();
-}

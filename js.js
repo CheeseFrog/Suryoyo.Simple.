d0067953bcda0685f5e07db4c175ffec00d2a5ee
CheeseFrog <au.yousef+dev@gmail.com>
2018-04-30 20:04:59 +1000

    Add files via upload

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 
 window.addEventListener("load",start);
-var y=[],x=[],v=0,l=[],l2=[];
+var y=[],x=[],v=0;
 var style = document.createElement('style');
 style.type = 'text/css';
 
@@ -18,30 +18,34 @@ var up = document.createElement("div");
 up.id="up";up.addEventListener("click",home,{passive: true});//up.innerText="△";
 document.body.appendChild(up)
 
-function getindex() {
 
 var ss=document.styleSheets[1];
 var css=(ss.cssRules?ss.cssRules:ss.rules);
-for (var i=0;i<css.length;i++) {if(css[i].style&&css[i].style.backgroundColor&&!css[i].style.padding) l.push(css[i].selectorText);}
+function classify(cl,nm,vl,ye,no) {
+var l=[];
+for (var i=0;i<css.length;i++) {
+if(css[i].style&&(ye?css[i].style[ye]&&!css[i].style[no]:css[i].style[nm]==vl)) l.push(css[i].selectorText);}
 for (var m=0;m<l.length;m++) {j=document.getElementsByClassName(l[m].substr(1));
-for (var k=0;k<j.length;k++) {j[k].classList.add("tit")}}}
+for (var k=0;k<j.length;k++) {j[k].classList.add(cl)}}
+return l;}
 
-function find() {
+function dexify(l) {
 for (var i=0;i<l.length;i++) y=y.concat([].slice.call(document.getElementsByClassName(l[i].replace(/\./g, ""))))
 for (var i=0;i<y.length;i++) {
 	var n=str(y[i].innerText);y[i].id=n;
 	y[i].dex=(n.charAt(0)==">")
-	y[i].onclick=(y[i].dex?scr:scr);
+	y[i].onclick=(y[i].dex?scr:scr);}}
+
+
+classify("ul","text-decoration","underline");
+classify("sup","vertical-align","super");
+dexify(classify("tit",0,0,"backgroundColor","padding"));
+document.getElementById("[Suryoyo.Simple.]").onclick=invert;
+}
 
 	//if (n) var last=document.getElementById(n);
 	//y[i].id=n;
 		//if (last) {last.id=""; }
-}}
-
-getindex();find();
-document.body.firstChild.firstChild.onclick=invert
-}
-
 //includeHTML()
 //x=document.getElementsByTagName("span");
 // function findold() {
